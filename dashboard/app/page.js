904296ae8b6cd946ee55f25d30195a6d073ca70f
Нүүдээ Баянбаатар <baataraa.contact@gmail.com>
2024-03-23 14:31:59 +0800

    First commit

---
+++ dashboard/app/page.js
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+       <h1>Hello world</h1>
+      </div>
+      <div className={styles.grid}>
+      </div>
+    </main>
+  );
+}
